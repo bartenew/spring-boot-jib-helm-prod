38646c933bb8cc3170bcac49f4523ef9d7ee8894
Arseniy A <bartenew@Arseniys-MacBook-Pro.local>
2020-02-25 19:10:34 -0800

    Init

---
+++ src/main/java/me/bartenew/haiku/HaikuApplication.java
@@ -0,0 +1,13 @@
+package me.bartenew.haiku;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HaikuApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HaikuApplication.class, args);
+    }
+
+}
